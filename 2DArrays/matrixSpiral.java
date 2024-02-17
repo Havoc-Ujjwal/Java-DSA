6cb03b72d95775f5b8dd9ccbb52ec03471ec2a3f
--replace-all <--replace-all>
2024-02-17 09:00:09 +0530

    function added in matrixSpiral for diagonal search

--- 2DArrays/matrixSpiral.java
+++ 2DArrays/matrixSpiral.java
@@ -1,4 +1,5 @@
 public class matrixSpiral {
+
     public static void printSpiral(int matrix[][]){
         int startRow = 0;
         int startCol = 0;
@@ -34,12 +35,24 @@ public class matrixSpiral {
             endCol--;
         }
     }
+
+    public static int diagonalSum(int matrix[][]){
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            sum+= matrix[i][i];
+            if(i != matrix.length-1-i){
+                sum+= matrix[i][matrix.length-i-1];
+            }
+        }
+
+        return sum;
+    }
     public static void main(String args[]){
         int matrix[][] = {{1, 2, 3, 4},
                           {5, 6, 7, 8},
                           {9, 10, 11, 12},
                           {13, 14, 15, 16}};
 
-        printSpiral(matrix);
+    System.out.println(diagonalSum(matrix));
     }
 }
